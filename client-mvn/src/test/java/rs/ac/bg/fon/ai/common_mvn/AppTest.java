f7f6911334b08ea29a2921e3d1a12295862d630c
vsonja <153772075+vsonja@users.noreply.github.com>
2024-07-10 11:45:55 +0200

    Napravljeni projekti common-mvn, client-mvn i server-mvn.

---
+++ client-mvn/src/test/java/rs/ac/bg/fon/ai/common_mvn/AppTest.java
@@ -0,0 +1,20 @@
+package rs.ac.bg.fon.ai.common_mvn;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
