b1bc5d78733b0b9e2c5879ca4c90b00a68ee5508
vsonja <sonjav018@gmail.com>
2024-07-11 18:14:48 +0200

    U klijentskom programu dodata klasa za komunikaciju sa serverom i server.properties fajl.

---
+++ server-mvn/src/main/java/rs/ac/bg/fon/ai/server_mvn/main/Main.java
@@ -0,0 +1,15 @@
+package rs.ac.bg.fon.ai.server_mvn.main;
+
+import rs.ac.bg.fon.ai.server_mvn.form.FrmServer;
+
+/**
+ *
+ * @author Sonja
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        new FrmServer().setVisible(true);
+    }
+
+}
