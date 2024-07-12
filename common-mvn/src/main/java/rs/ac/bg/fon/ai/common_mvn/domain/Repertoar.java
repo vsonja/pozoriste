cdb872ee5bf70a553afc375ab6a894eb0f753595
vsonja <sonjav018@gmail.com>
2024-07-12 18:40:28 +0200

    Dokumentovana klasa Repertoar.

--- common-mvn/src/main/java/rs/ac/bg/fon/ai/common_mvn/domain/Repertoar.java
+++ common-mvn/src/main/java/rs/ac/bg/fon/ai/common_mvn/domain/Repertoar.java
@@ -8,19 +8,52 @@ import java.sql.SQLException;
 import java.util.List;
 
 /**
+ * Predstavlja mesecni repertoar pozorista.
+ * Repertoar ima repertoarID, mesec, godinu i listu stavki repertoara.
+ * 
+ * Ova klasa nasledjuje apstraktnu klasu OpstiDomenskiObjekat i implementira interfejs Serializable, jer se podaci salju kroz mrezu.
  *
  * @author Sonja
+ * @version 1.0
  */
 public class Repertoar extends OpstiDomenskiObjekat implements Serializable {
 
+	/**
+	 * Jedinstveni identifikator repertoara kao Integer.
+	 */
     private Integer repertoarID;
+    
+    /**
+     * Mesec na koji se repertoar odnosi kao int.
+     */
     private int mesec;
+    
+    /**
+     * Godina na koju se repertoar odnosi kao int.
+     */
     private int godina;
+    
+    /**
+     * Stavke repertoara kao lista objekata klase StavkaRepertoara.
+     */
     List<StavkaRepertoara> stavke;
 
+    /**
+     * Pravi nov objekat klase Repertoar.
+     * 
+     * Svi njegovi atributi (repertoarID, mesec, godina i stavke) ostaju neinicijalizovani.
+     */
     public Repertoar() {
     }
 
+    /**
+     * Pravi novi repertoar i postavlja jedinstveni identifikator repertoara, mesec, godinu i stavke na unete vrednosti.
+     * 
+     * @param repertoarID Jedinstveni identifikator repertoara kao Integer.
+     * @param mesec Mesec na koji se repertoar odnosi kao int.
+     * @param godina Godina na koju se repertoar odnosi kao int.
+     * @param stavke Stavke repertoara kao lista objekata klase StavkaRepertoara.
+     */
     public Repertoar(Integer repertoarID, int mesec, int godina, List<StavkaRepertoara> stavke) {
         this.repertoarID = repertoarID;
         this.mesec = mesec;
@@ -28,34 +61,74 @@ public class Repertoar extends OpstiDomenskiObjekat implements Serializable {
         this.stavke = stavke;
     }
 
+    /**
+     * Vraca godinu na koju se repertoar odnosi.
+     * 
+     * @return Godina na koju se repertoar odnosi kao int.
+     */
     public int getGodina() {
         return godina;
     }
 
+    /**
+     * Postavlja godinu na koju se repertoar odnosi na unetu vrednost.
+     * 
+     * @param godina Godina na koju se repertoar odnosi kao int.
+     */
     public void setGodina(int godina) {
         this.godina = godina;
     }
 
+    /**
+     * Vraca jedinstveni identifikator repertoara.
+     * 
+     * @return Trenutni identifikator repertoara kao Integer.
+     */
     public Integer getRepertoarID() {
         return repertoarID;
     }
 
+    /**
+     * Postavlja jedinstveni identifikator repertoara na unetu vrednost.
+	 * 
+     * @param repertoarID Jedinstveni identifikator repertoara kao Integer.
+     */
     public void setRepertoarID(Integer repertoarID) {
         this.repertoarID = repertoarID;
     }
 
+    /**
+     * Vraca mesec na koji se repertoar odnosi.
+     * 
+     * @return Mesec na koji se repertoar odnosi kao int.
+     */
     public int getMesec() {
         return mesec;
     }
 
+    /**
+     * Postavlja mesec na koji se repertoar odnosi na unetu vrednost.
+     * 
+     * @param mesec Mesec na koji se repertoar odnosi kao int.
+     */
     public void setMesec(int mesec) {
         this.mesec = mesec;
     }
 
+    /**
+     * Vraca listu stavki repertoara.
+     * 
+     * @return Trenutne stavke repertoara kao lista objekata klase StavkaRepertoara.
+     */
     public List<StavkaRepertoara> getStavke() {
         return stavke;
     }
 
+    /**
+     * Postavlja listu stavki repertoara na unetu vrednost.
+     * 
+     * @param stavke Lista objekata klase StavkaRepertoara.
+     */
     public void setStavke(List<StavkaRepertoara> stavke) {
         this.stavke = stavke;
     }
