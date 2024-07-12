aa626d20329ee511359b852d6ca9fe781117b0ed
vsonja <sonjav018@gmail.com>
2024-07-12 19:07:08 +0200

    Dokumentovana klasa StavkaRepertoara.

--- common-mvn/src/main/java/rs/ac/bg/fon/ai/common_mvn/domain/StavkaRepertoara.java
+++ common-mvn/src/main/java/rs/ac/bg/fon/ai/common_mvn/domain/StavkaRepertoara.java
@@ -11,22 +11,68 @@ import java.time.LocalTime;
 import java.util.List;
 
 /**
+ * Predstavlja stavku repertoara, odnosno konkretno izvodjenje predstave.
+ * Stavka repertoara ima stavkaRepertoaraID, repertoar kom pripada, datum, vreme pocetka, vreme zavrsetka i predstavu koja se izvodi.
  *
  * @author Sonja
+ * @version 1.0
  */
 public class StavkaRepertoara extends OpstiDomenskiObjekat implements Serializable {
 
+	/**
+	 * Jedinstveni identifikator stavke repertoara kao Integer.
+	 */
     private Integer stavkaRepertoaraID;
+    
+    /**
+     * Repertoar kom stavka pripada kao objekat klase Repertoar.
+     */
     private Repertoar repertoar;
+    
+    /**
+     * Datum konkretnog izvodjenja predstave kao LocalDate.
+     */
     private LocalDate datum;
+    
+    /**
+     * Vreme pocetka konkretnog izvodjenja predstave kao LocalTime.
+     */
     private LocalTime vremePocetka;
+    
+    /**
+     * Vreme zavrsetka konkretnog izvodjenja predstave kao LocalTime.
+     */
     private LocalTime vremeZavrsetka;
+    
+    /**
+     * Predstava koja se izvodi kao objekat klase Predstava.
+     */
     private Predstava predstava;
-    Sala sala;
+    
+    /**
+     * Sala u kojoj se izvodi predstava kao objekat klase Sala.
+     */
+    private Sala sala;
 
+    /**
+     * Pravi nov objekat klase StavkaRepertoara.
+     * 
+     * Svi njegovi atributi ostaju neinicijalizovani.
+     */
     public StavkaRepertoara() {
     }
 
+    /**
+     * Pravi nov objekat klase StavkaRepertoara i postavlja vrednosti svih atributa.
+     * 
+     * @param stavkaRepertoaraID Jedinstveni identifikator stavke repertoara kao Integer.
+     * @param repertoar Repertoar kom stavka pripada kao objekat klase Repertoar.
+     * @param datum Datum konkretnog izvodjenja predstave kao LocalDate.
+     * @param vremePocetka Vreme pocetka konkretnog izvodjenja predstave kao LocalTime.
+     * @param vremeZavrsetka Vreme zavrsetka konkretnog izvodjenja predstave kao LocalTime.
+     * @param predstava Predstava koja se izvodi kao objekat klase Predstava.
+     * @param sala Sala u kojoj se izvodi predstava kao objekat klase Sala.
+     */
     public StavkaRepertoara(Integer stavkaRepertoaraID, Repertoar repertoar, LocalDate datum, LocalTime vremePocetka, LocalTime vremeZavrsetka, Predstava predstava, Sala sala) {
         this.stavkaRepertoaraID = stavkaRepertoaraID;
         this.repertoar = repertoar;
@@ -37,58 +83,128 @@ public class StavkaRepertoara extends OpstiDomenskiObjekat implements Serializab
         this.sala = sala;
     }
 
+    /**
+     * Vraca predstavu na koju se odnosi stavka repertoara.
+     * 
+     * @return Predstava na koju se odnosi stavka repertoara kao objekat klase Predstava.
+     */
     public Predstava getPredstava() {
         return predstava;
     }
 
+    /**
+     * Postavlja vrednost predstave na koju se odnosi stavka repertoara na unetu vrednost.
+     * 
+     * @param predstava Predstava na koju se odnosi stavka repertoara kao objekat klase Predstava.
+     */
     public void setPredstava(Predstava predstava) {
         this.predstava = predstava;
     }
 
+    /**
+     * Vraca repertoar kom pripada stavka repertoara.
+     * 
+     * @return Repertoar kom pripada stavka repertoara kao objekat klase Repertoar.
+     */
     public Repertoar getRepertoar() {
         return repertoar;
     }
 
+    /**
+     * Postavlja vrednost repertoar kom pripada stavka repertoara na unetu vrednost.
+     * 
+     * @param repertoar Repertoar kom pripada stavka repertoara kao objekat klase Repertoar.
+     */
     public void setRepertoar(Repertoar repertoar) {
         this.repertoar = repertoar;
     }
 
+    /**
+     * Vraca salu u kojoj se izvodi predstava.
+     * 
+     * @return Sala u kojoj se izvodi predstava kao objekat klase Sala.
+     */
     public Sala getSala() {
         return sala;
     }
 
+    /**
+     * Postavlja salu na unetu vrednost.
+     * 
+     * @param sala Sala u kojoj se izvodi predstava kao objekat klase Sala.
+     */
     public void setSala(Sala sala) {
         this.sala = sala;
     }
 
+    /**
+     * Vraca jedinstveni identifikator stavke repertoara.
+     * 
+     * @return Trenutni identifikator stavke repertoara kao Integer.
+     */
     public Integer getStavkaRepertoaraID() {
         return stavkaRepertoaraID;
     }
-
+    
+    /**
+     * Postavlja jedinstveni identifikator stavke repertoara na unetu vrednost.
+	 * 
+     * @param stavkaRepertoaraID Jedinstveni identifikator stavke repertoara kao Integer.
+     */
     public void setStavkaRepertoaraID(Integer stavkaRepertoaraID) {
         this.stavkaRepertoaraID = stavkaRepertoaraID;
     }
 
+    /**
+     * Vraca datum konkretnog izvodjenja predstave.
+     * 
+     * @return Datum konkretnog izvodjenja predstave kao LocalDate.
+     */
     public LocalDate getDatum() {
         return datum;
     }
 
+    /**
+     * Postavlja datum na unetu vrednost.
+     * 
+     * @param datum datum konkretnog izvodjenja predstave kao LocalDate.
+     */
     public void setDatum(LocalDate datum) {
         this.datum = datum;
     }
 
+    /**
+     * Vraca vreme pocetka konkretnog izvodjenja predstave.
+     * 
+     * @return Vreme pocetka konkretnog izvodjenja predstave kao LocalTime.
+     */
     public LocalTime getVremePocetka() {
         return vremePocetka;
     }
 
+    /**
+     * Postavlja vreme pocetka na unetu vrednost.
+     * 
+     * @param vremePocetka Vreme pocetka konkretnog izvodjenja predstave kao LocalTime.
+     */
     public void setVremePocetka(LocalTime vremePocetka) {
         this.vremePocetka = vremePocetka;
     }
 
+    /**
+     * Vraca vreme zavrsetka konkretnog izvodjenja predstave.
+     * 
+     * @return Vreme zavrsetka konkretnog izvodjenja predstave kao LocalTime.
+     */
     public LocalTime getVremeZavrsetka() {
         return vremeZavrsetka;
     }
 
+    /**
+     * Postavlja vreme zavrsetka na unetu vrednost.
+     * 
+     * @param vremeZavrsetka Vreme zavrsetka konkretnog izvodjenja predstave kao LocalTime.
+     */
     public void setVremeZavrsetka(LocalTime vremeZavrsetka) {
         this.vremeZavrsetka = vremeZavrsetka;
     }
