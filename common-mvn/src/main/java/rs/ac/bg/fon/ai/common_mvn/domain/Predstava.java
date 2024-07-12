5fbe395cd5edb21f23b1af16e6527bdd64e6cbf8
vsonja <sonjav018@gmail.com>
2024-07-12 18:22:18 +0200

    Dokumentovana klasa Predstava.

--- common-mvn/src/main/java/rs/ac/bg/fon/ai/common_mvn/domain/Predstava.java
+++ common-mvn/src/main/java/rs/ac/bg/fon/ai/common_mvn/domain/Predstava.java
@@ -9,55 +9,128 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * Predstavlja predstavu koja je na repertoaru pozorista.
+ * Predstava ima predstavaID, naziv, opis i listu uloga.
+ * 
+ * Ova klasa nasledjuje apstraktnu klasu OpstiDomenskiObjekat i implementira interfejs Serializable, jer se podaci salju kroz mrezu.
  *
  * @author Sonja
+ * @version 1.0
  */
 public class Predstava extends OpstiDomenskiObjekat implements Serializable {
 
+	/**
+	 * Jedinstveni identifikator predstave kao Integer.
+	 */
     private Integer predstavaID;
+    
+    /**
+     * Naziv predstave kao String.
+     */
     private String nazivPredstave;
+    
+    /**
+     * Opis predstave kao String.
+     */
     private String opis;
+    
+    /**
+     * Uloge u predstavi kao lista objekata klase Uloga.
+     */
     private List<Uloga> uloge;
 
+    /**
+     * Pravi nov objekat klase Predstava.
+     * 
+     * Svi njegovi atributi (predstavaID, nazivPredstave, opis) osim liste uloga ostaju neinicijalizovani.
+     */
     public Predstava() {
         uloge = new ArrayList<>();
     }
 
+    /**
+     * Pravi novu predstavu i postavlja jedinstveni identifikator predstave, naziv, opis i uloge na unete vrednosti.
+     * 
+     * @param predstavaID Jedinstveni identifikator predstave kao Integer.
+     * @param nazivPredstave Naziv predstave kao String.
+     * @param opis Opis predstave kao String.
+     * @param uloge Uloge u predstavi kao lista objekata klase Uloga.
+     */
     public Predstava(Integer predstavaID, String nazivPredstave, String opis, List<Uloga> uloge) {
         this.predstavaID = predstavaID;
         this.nazivPredstave = nazivPredstave;
         this.opis = opis;
         this.uloge = uloge;
     }
-
+    
+    /**
+     * Vraca listu uloga u predstavi.
+     * 
+     * @return Trenutne uloge u predstavi kao lista objekata klase Uloga.
+     */
     public List<Uloga> getUloge() {
         return uloge;
     }
 
+    /**
+     * Postavlja listu uloga u predstavi na unetu vrednost.
+     * 
+     * @param uloge Lista objekata klase Uloga.
+     */
     public void setUloge(List<Uloga> uloge) {
         this.uloge = uloge;
     }
 
+    /**
+     * Vraca jedinstveni identifikator predstave.
+     * 
+     * @return Trenutni identifikator predstave kao Integer.
+     */
     public Integer getPredstavaID() {
         return predstavaID;
     }
 
+    /**
+     * Postavlja jedinstveni identifikator predstave na unetu vrednost.
+	 * 
+     * @param predstavaID Jedinstveni identifikator predstave kao Integer.
+     */
     public void setPredstavaID(Integer predstavaID) {
         this.predstavaID = predstavaID;
     }
 
+    /**
+     * Vraca naziv predstave.
+     * 
+     * @return Trenutni naziv predstave kao String.
+     */
     public String getNazivPredstave() {
         return nazivPredstave;
     }
 
+    /**
+     * Postavlja naziv predstave na unetu vrednost.
+     * 
+     * @param nazivPredstave Naziv predstave kao String.
+     */
     public void setNazivPredstave(String nazivPredstave) {
         this.nazivPredstave = nazivPredstave;
     }
 
+    /**
+     * Vraca opis predstave.
+     * 
+     * @return Trenutni opis predstave kao String.
+     */
     public String getOpis() {
         return opis;
     }
 
+    /**
+     * Postavlja opis predstave na unetu vrednost.
+     * 
+     * @param opis Opis predstave kao String.
+     */
     public void setOpis(String opis) {
         this.opis = opis;
     }
